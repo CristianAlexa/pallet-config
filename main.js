076bc74a87fac1c1f183abb9a1efb7717028ec83
CristianAlexa <cristian.alexa@gmail.com>
2024-11-13 13:05:18 +0000

    html & css

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+const domElements = {
+    palletFormEl: document.getElementById('palletForm'),
+    caseWidthEl: document.getElementById('caseWidth'),
+    caseLengthEl: document.getElementById('caseLength'),
+    caseHeightEl: document.getElementById('caseHeight'),
+    palletSizeEl: document.getElementById('palletSize'),
+    loadHeightEl: document.getElementById('loadHeight')
+}
 
+
+domElements.palletFormEl.addEventListener('submit', (e) => {
+    e.preventDefault()
+    console.log('clicked')
+    
+})
